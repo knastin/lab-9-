e33879823ed8ac52e3c3a69e7e2edbfd5f9e70d2
knastin <knastin@users.noreply.github.com>
2015-09-24 21:41:17 +0300

    Update App.java

--- src/main/java/ru/spb/herzen/ivt3/App.java
+++ src/main/java/ru/spb/herzen/ivt3/App.java
@@ -2,16 +2,26 @@ package ru.spb.herzen.ivt3;
 
 import org.kohsuke.randname.RandomNameGenerator;
 
-/**
- * Hello world!
- *
- */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        RandomNameGenerator rnd = new RandomNameGenerator(2015);
-        String name = rnd.next();
-        System.out.println("Hello, "+name);
+        System.out.println();
+        RandomNameGenerator rand = new RandomNameGenerator();
+
+        String name = rand.next();
+        System.out.println( "Hello, " + name + "\n" );
+
+        System.out.println( "Задание 1" );
+        for(int i=0; i<4; i++){
+            name = rand.next();
+            System.out.println( "Hello, " + name );
+        }
+
+        System.out.println( "\nЗадание 2" );
+        for(int i=0; i<15; i++){
+            name = rand.next();
+            System.out.println( "Hello, " + name );
+        }
     }
 }
